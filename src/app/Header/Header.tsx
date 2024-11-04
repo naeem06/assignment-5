17805a836fa0170cbdb5cd1d766431bff7b436bc
naeem06 <mn041009@gmail.com>
2024-11-04 14:11:55 -0800

    add

---
+++ src/app/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+   <div>
+    <div id='navbar' className='bg-[#A29875] w-[100%] h-[100px] flex text-[30px] '>
+
+
+       <div id='logo' className='w-[242.56px] h-[87.45px] flex pt-[23.22px] pl-[56.76px] text-[#FFFFFF] font-[400] font-[rye]'><h1>MANZZARI</h1>
+       </div>
+
+    </div>
+   </div>
+  )
+}
+
+export default Header
